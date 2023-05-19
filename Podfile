73885b1e86a0d8cb4e19d00762a8f874127386a8
22yuzu <yuzubook2@icloud.com>
2023-05-19 11:22:11 +0900

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'mustholic1' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for mustholic1
+
+  target 'mustholic1Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'mustholic1UITests' do
+    # Pods for testing
+  end
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+  pod 'FirebaseAnalytics'
+  pod 'IQKeyboardManagerSwift'
+
+end
---
+++ mustholic1/mustholic1App.swift
@@ -0,0 +1,37 @@
+//
+//  mustholic1App.swift
+//  mustholic1
+//
+//  Created by 冨岡哲平 on 2023/05/19.
+//
+
+import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseAnalytics
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
